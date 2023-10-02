c33e9b481992cd51090f1f5f50e8301cf069043b
Jenny <jenny.maya186@gmail.com>
2023-10-02 17:08:58 +0700

    Updated

---
+++ get-ride/Modal/Profile.swift
@@ -0,0 +1,8 @@
+//
+//  Profile.swift
+//  get-ride
+//
+//  Created by Janny on 2/10/23.
+//
+
+import Foundation
---
+++ get-ride/Requests/LoginRequest.swift
@@ -0,0 +1,8 @@
+//
+//  LoginRequest.swift
+//  get-ride
+//
+//  Created by Janny on 2/10/23.
+//
+
+import Foundation
